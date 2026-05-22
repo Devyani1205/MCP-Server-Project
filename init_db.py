ded674b1dfd423a3b0f3d7c4ca755998fdfdb9e8
Devyani Seth <61617896+Devyani1205@users.noreply.github.com>
2026-05-22 10:58:15 +0530

    Delete init_db.py

--- init_db.py
+++
@@ -1,7 +0,0 @@
-from app.database import init_db, seed_diagnostic_tests
-
-if __name__ == "__main__":
-    # 1. Create tables and seed (init_db calls seed_users and seed_diagnostic_tests)
-    init_db()
-    
-    print("Database seeding process completed.")
